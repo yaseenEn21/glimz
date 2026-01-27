e08adef3b7ea6a67a945eeb312262eee27d22feb
Yaseen Enaya <yenaya15@gmail.com>
2026-01-27 20:31:15 +0200

    update translate

--- resources/lang/ar/payment_purposes.php
+++ resources/lang/ar/payment_purposes.php
@@ -4,6 +4,7 @@ return [
     'wallet_topup'     => 'شحن المحفظة',
     'invoice_payment'  => 'سداد فاتورة',
     'booking_payment'  => 'سداد حجز',
+    'booking_invoice'  => 'فاتورة حجز',
     'package_purchase' => 'شراء باقة',
     'refund'           => 'استرجاع',
     'other'            => 'اخرى',
