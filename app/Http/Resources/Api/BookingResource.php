cad840963da4b8fdfad317bcb4af8418501284bd
Yaseen Enaya <yenaya15@gmail.com>
2026-02-09 19:43:42 +0200

    add datetime to booking resource

--- app/Http/Resources/Api/BookingResource.php
+++ app/Http/Resources/Api/BookingResource.php
@@ -27,7 +27,7 @@ class BookingResource extends JsonResource
                 ? $this->booking_date
                     ->copy()
                     ->setTimeFromTimeString($this->start_time)
-                    ->format('Y-m-d\TH:i:s')
+                    ->toIso8601String()
                 : null,
             'start_time' => substr((string) $this->start_time, 0, 5),
             'end_time' => substr((string) $this->end_time, 0, 5),
